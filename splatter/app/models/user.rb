4c876560cce4996f235c24d4e9286102c59de368
trengcj1 <ctrengrove@hotmail.co.nz>
2014-08-05 22:48:03 +0000

    testing 6th august

--- splatter/app/models/user.rb
+++ splatter/app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ActiveRecord::Base
-has_many :splatts
-has_and_belongs_to_many :follows,
-class_name: "User",
-join_table: :follows,
-foreign_key: :follower_id,
-association_foreign_key: :followed_id
-
+	has_many :splatts
+	has_and_belongs_to_many :follows,
+	class_name: "User",
+	join_table: :follows,
+	foreign_key: :follower_id,
+	association_foreign_key: :followed_id
 end
