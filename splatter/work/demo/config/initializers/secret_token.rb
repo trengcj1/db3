d6f4561feec2fcda764475274c982eb4cb5376e3
trengrove <ctrengrove@hotmail.co.nz>
2014-09-15 11:53:20 +0000

    test

---
+++ splatter/work/demo/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demo::Application.config.secret_key_base = 'd7f3fdeccde954ab71a087f4f6b831686ba53fd9af3c177e5dec72bc108be39748a07f226fb3ec7d3c976ad3916ed3c1665f0e92bc324ec2893ed7fb7d35388b'
